27fe3384edb5ab1863c09643e6fafd015c1058ae
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-26 21:11:13 +0100

    create database for bitcoin

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pandas as pd
 import numpy as np
 import matplotlib as mtp
+import sqlite3
+
+conn = sqlite3.connect('Bitcoin_price.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE Prices_Bitcoin (
+    Coin TEXT,
+    Date DATETIME,
+    Currency TEXT,
+    Price INTEGER,
+    PRIMARY KEY (coin, date, currency)
+)
+''')
+
+conn.commit()
+conn.close()
