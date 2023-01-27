93c86b5e5343fa83e66555c033dfac3de34f348b
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-27 14:39:20 +0100

    start creating desktop module with pyside6

--- plot.py
+++ plot.py
@@ -8,3 +8,11 @@ df.plot()
 plt.show()
 
 #print(df)
+
+# print(df.tail(1)[['Date']]) -> last download line from db
+
+#first_row = conn.execute("SELECT * FROM `PriceBitcoins` ORDER BY date LIMIT 1;").fetchone()
+#last_row = conn.execute("SELECT * FROM `PriceBitcoins` ORDER BY date DESC LIMIT 1;").fetchone()
+#print("First row:", first_row)
+#print("Last row:", last_row)
+#conn.close()
