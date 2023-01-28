5760740ed6058fa006fee5fe693d13d7bebc05cd
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-28 12:58:41 +0100

    update right_wind section with download data fr db

--- guibtc.py
+++ guibtc.py
@@ -2,16 +2,12 @@ from PySide6.QtWidgets import QApplication, QVBoxLayout, QHBoxLayout, QPushButto
 import pandas as pd
 import sqlite3
 import matplotlib.pyplot as plt
+import matplotlib.widgets as widgets
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
-
-        conn = sqlite3.connect('mydatabitcoins.db')
-        self.df = pd.read_sql('select * from PriceBitcoins', conn)
-        #df.plot()
-        #plt.show()
-
+        
         self.main_layout = QHBoxLayout()
         self.left_layout = QVBoxLayout()
         self.button1 = QPushButton("REFRESH")
@@ -31,9 +27,10 @@ class MyWindow(QWidget):
         self.button4.setStyleSheet("background-color: red; color: white; font-weight: bold;")
         self.button5.setStyleSheet("background-color: red; color: white; font-weight: bold;")
 
+        self.conn = sqlite3.connect('mydatabitcoins.db')
+        self.df = pd.read_sql('select * from PriceBitcoins', self.conn)
+    
         self.right_widget = QTextEdit()
-        #for i in range(10):
-            #right_widget.append("Linia tekstu {}".format(i+1))
 
         self.main_layout.addLayout(self.left_layout)
         self.main_layout.addWidget(self.right_widget)
@@ -43,10 +40,24 @@ class MyWindow(QWidget):
         self.resize(700,400)
         self.setStyleSheet("background-color: black; color: white;")
         
+        self.button1.clicked.connect(self.news_right_wind)
         self.button2.clicked.connect(self.on_button_pie)
+        self.button3.clicked.connect(self.on_button_bar)
+        self.button4.clicked.connect(self.on_button_line)
         self.button5.clicked.connect(self.on_button_clicked)
         
-        conn.close()
+    def news_right_wind(self):
+        
+        self.conn = sqlite3.connect('mydatabitcoins.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('SELECT * FROM PriceBitcoins')
+        self.results = self.cursor.fetchall()
+        
+        for result in self.results:
+           self.right_widget.append(result[1])
+        
+        self.conn.close()
+        
 
     def on_button_clicked(self):
         app.exit()
@@ -54,9 +65,28 @@ class MyWindow(QWidget):
     def on_button_pie(self):
         conn = sqlite3.connect('mydatabitcoins.db')
         self.df = pd.read_sql('select * from PriceBitcoins', conn)
+        self.df = self.df.head(50)
         self.df.plot()
         plt.show()
         conn.close()
+        
+    def on_button_line(self):
+        conn = sqlite3.connect('mydatabitcoins.db')
+        self.df = pd.read_sql('select * from PriceBitcoins', conn)
+        self.df = self.df['Price'].value_counts().nlargest(30)
+        self.df.plot.bar(x='Price', y='Date')
+        plt.show()
+        conn.close()
+    
+    def on_button_bar(self):
+        conn = sqlite3.connect('mydatabitcoins.db')
+        self.df = pd.read_sql('select * from PriceBitcoins', conn)
+        self.df = self.df['Price'].value_counts().nlargest(20)
+        self.df.plot.pie(y='Price', autopct='%1.1f%%')
+        plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+        plt.show()
+        conn.close()
+        
 
 app = QApplication()
 window = MyWindow()
